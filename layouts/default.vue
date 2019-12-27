c96eaa44bf017b91fb4d9dd0b95bbc567dc6f129
daigo <hal.60445.kobayashi@gmail.com>
2019-12-27 13:39:01 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <Bottom></Bottom>
+  </v-app>
+</template>
+
+<script>
+import Bottom from "~/components/bottom.vue";
+
+export default {
+  data () {
+    return {
+      title: 'Vuetify.js'
+    }
+  },
+  components:{
+    Bottom
+  }
+}
+</script>
