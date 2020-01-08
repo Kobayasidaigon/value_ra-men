24539beadef710e3cdde32c445a3fab7bf2a962f
daigo <hal.60445.kobayashi@gmail.com>
2020-01-08 17:54:02 +0900

    splashscreen,iconの追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,10 +11,21 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'apple-mobile-web-app-capable',content="yes"}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { href:"img/iphone5_splash.png", media:"(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)", rel:"apple-touch-startup-image"},
+      { href:"img/iphone6_splash.png", media:"(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)", rel:"apple-touch-startup-image"},
+      { href:"img/iphoneplus_splash.png", media:"(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)", rel:"apple-touch-startup-image"},
+      { href:"img/iphonex_splash.png", media:"(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)", rel:"apple-touch-startup-image"},
+      { href:"img/iphonexr_splash.png", media:"(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)", rel:"apple-touch-startup-image"},
+      { href:"img/iphonexsmax_splash.png", media:"(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)", rel:"apple-touch-startup-image"},
+      { href:"img/ipad_splash.png", media:"(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)", rel:"apple-touch-startup-image"},
+      { href:"img/ipadpro1_splash.png", media:"(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)", rel:"apple-touch-startup-image"},
+      { href:"img/ipadpro3_splash.png", media:"(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)", rel:"apple-touch-startup-image"},
+
     ]
   },
   /*
@@ -77,6 +88,7 @@ export default {
   manifest:{  
     name: "ラーメン",
     title: "ラーメン",
+    description: "",
     lang: 'ja',
     theme_color: "#529b58",
     background_color: "#bde0c0",
